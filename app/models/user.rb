e2225b5c3ebabcb6931f48e4da4bdc7025280b03
Dmitry Vorontsov <vorontsof@mail.ru>
2020-01-16 00:28:25 +0400

    'tada'lesson5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+
   def tests_by_level(level)
     Test.joins('join passed_tests on passed_tests.test_id = tests.id')
         .where(passed_tests: { user_id: self.id }, level: level)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_075303) do
+ActiveRecord::Schema.define(version: 2020_01_15_191710) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2020_01_14_075303) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
