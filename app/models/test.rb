323ed6b3a84650f80b3f4b5f740cfa93099f8f9d
Dmitry Vorontsov <vorontsof@mail.ru>
2020-01-17 13:09:11 +0400

    lesson5 update, author added

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :questions
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
   def self.sort_by_category(category)
-    Test.joins("JOIN categories ON categories.id = tests.category_id")
-    .where("categories.title = ?", category).order(title: :desc).pluck(:title)
+    # Test.joins("JOIN categories ON categories.id = tests.category_id")
+    # .where("categories.title = ?", category).order(title: :desc).pluck(:title)
+    Test.joins(:category).where(categories: {title: category}).order(title: :desc)
   end
 end
